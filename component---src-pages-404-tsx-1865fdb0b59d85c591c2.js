cdd6e6ab1f4ffa9f1bd5c335f74a73134637bfe7
seelx3 <seelx3@gmail.com>
2024-11-23 02:03:53 +0900

    Updates

---
+++ component---src-pages-404-tsx-1865fdb0b59d85c591c2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[453],{731:function(e,n,l){l.r(n),l.d(n,{Head:function(){return o}});var t=l(6540),u=l(4794);n.default=()=>t.createElement("main",null,t.createElement("h1",null,"Page not found"),t.createElement("p",null,"Sorry 😔, we couldn't find what you were looking for.",t.createElement("br",null),null,t.createElement("br",null),t.createElement(u.Link,{to:"/"},"Go home"),"."));const o=()=>t.createElement("title",null,"Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-1865fdb0b59d85c591c2.js.map
